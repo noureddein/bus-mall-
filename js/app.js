7c2c9ae46cb144d5a9af6246006dc2eb32895ce8
noureddein <noureddein@gmail.com>
2021-02-22 16:45:44 +0200

    notSolved

--- js/app.js
+++ js/app.js
@@ -23,14 +23,10 @@ const imgsName = [
 
 ];
 
+
 const leftImage = document.getElementById('left-image');
 const rightImage = document.getElementById('right-image');
 const middleImage = document.getElementById('middle-image');
-
-
-console.log(leftImage);
-console.log(rightImage);
-console.log(middleImage);
 
 
 function BusMall(imgsName) {
@@ -38,17 +34,57 @@ function BusMall(imgsName) {
     this.path = `./img/${imgsName}`;
     this.votes = 0;
     this.views = 0;
-    //BusMalls.push(this);
     BusMall.all.push(this);
 }
 BusMall.all = [];
 for (let i = 0; i < imgsName.length; i++) {
     new BusMall(imgsName[i]);
 }
-// console.table(BusMall.all);
 
+
+let renderedImgs = [];
+
+let secondIteration = [];
+let arrLeft = [];
+let arrMiddle = [];
+let arrRight = [];
 function renderImg() {
-    let renderedImgs = [];
+
+
+    for (let i = 0; i < 3; i++) {
+        arrLeft.push(randomNumber(0, BusMall.all.length - 1));
+        for ()
+    }
+
+    for (let i = 0; i < 5; i++) {
+        arrLeft.push(randomNumber(0, BusMall.all.length - 1));
+
+    }
+    console.log('Left', arrLeft);
+
+    for (let j = 0; j < 5; j++) {
+        arrMiddle.push(randomNumber(0, BusMall.all.length - 1));
+        for (let i = 0; i < 5; i++) {
+            if (arrMiddle[i] === arrLeft[j]) {
+                arrMiddle.pop();
+                arrMiddle.push(randomNumber(0, BusMall.all.length - 1));
+            }
+        }
+    }
+    console.log('Middle', arrMiddle);
+
+    for (let j = 0; j < 5; j++) {
+        arrRight.push(randomNumber(0, BusMall.all.length - 1));
+        for (let i = 0; i < 5; i++) {
+            if (arrRight[i] === arrLeft[j] && arrRight[i] === arrMiddle[j]) {
+                arrMiddle.pop();
+                arrMiddle.push(randomNumber(0, BusMall.all.length - 1));
+            }
+        }
+    }
+
+    console.log('Right', arrRight);
+
     const leftIndex = randomNumber(0, BusMall.all.length - 1);
 
     leftImage.src = BusMall.all[leftIndex].path;
@@ -56,12 +92,6 @@ function renderImg() {
     leftImage.alt = BusMall.all[leftIndex].name;
     renderedImgs.push(leftImage.alt);
 
-    const rightIndex = randomNumber(0, BusMall.all.length - 1);
-
-    rightImage.src = BusMall.all[rightIndex].path;
-    rightImage.title = BusMall.all[rightIndex].name;
-    rightImage.alt = BusMall.all[rightIndex].name;
-    renderedImgs.push(rightImage.alt);
 
     const middleIndex = randomNumber(0, BusMall.all.length - 1);
 
@@ -70,23 +100,37 @@ function renderImg() {
     middleImage.alt = BusMall.all[middleIndex].name;
     renderedImgs.push(middleImage.alt);
 
+    const rightIndex = randomNumber(0, BusMall.all.length - 1);
+
+    rightImage.src = BusMall.all[rightIndex].path;
+    rightImage.title = BusMall.all[rightIndex].name;
+    rightImage.alt = BusMall.all[rightIndex].name;
+    renderedImgs.push(rightImage.alt);
+
     for (let i = 0; i < BusMall.all.length; i++) {
         for (let j = 0; j < renderedImgs.length; j++) {
-            console.log(renderedImgs[j]);
-            console.log(BusMall.all[i].name);
+
             if (BusMall.all[i].name === renderedImgs[j]) {
                 BusMall.all[i].views = BusMall.all[i].views + 1;
             }
         }
     }
-    console.log(BusMall.all);
-    console.log(renderedImgs);
+
 }
 
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 renderImg();
+
+
+
+
+
+
+
+
+
 
 function renderResults() {
     const results = document.getElementById('left-side');
@@ -105,27 +149,61 @@ const imagesSection = document.getElementById('right-side');
 
 imagesSection.addEventListener('click', handleClick);
 
-let x = 0;
-let attempts = 25;
+let x = 1;
+let attempts = 5;
+
 function handleClick(event) {
 
-    if (x === attempts - 1) {
-        imagesSection.removeEventListener('click', handleClick);
-        renderResults();
-    }
-    if (event.returnValue === true) {
-        x++;
+
+    console.log('Before Delete renderedImgs; ', renderedImgs);
+    secondIteration = renderedImgs;
+    console.log('After add rendered to seconIreration ', secondIteration);
+    renderedImgs = new Array();
+    console.log('After Delete renderedImg', renderedImgs);
+
+
+    renderImg();
+    renderedImgs.push(leftImage.alt);
+    renderedImgs.push(middleImage.alt);
+    renderedImgs.push(rightImage.alt);
+    console.log('after Push', renderedImgs);
+
+    for (let i = 0; i < renderedImgs.length; i++) {
+        for (let j = 0; j < secondIteration.length; j++) {
+            if (secondIteration[j] === renderedImgs[i]) {
+                renderImg;
+            }
+        }
     }
     if (event.target.id !== 'right-side') {
         for (let i = 0; i < BusMall.all.length; i++) {
             if (BusMall.all[i].name === event.target.title) {
                 BusMall.all[i].votes = BusMall.all[i].votes + 1;
+
             }
         }
-        renderImg();
-        console.log(BusMall.all);
         event.preventDefault();
+
     }
+
+    if (x !== attempts) {
+        x++;
+    } else {
+        imagesSection.removeEventListener('click', handleClick);
+        renderResults();
+    }
+    renderedImgs = new Array();
+    console.log('renderedImgs', renderedImgs);
+
+
+
+
 }
+
+
+
+
+
+
 
 
